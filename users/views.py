ad977818aed41b1e124ddc79c4cdacdcccc3f04e
hyeon21@hufs.ac.kr <hyeon21@hufs.ac.kr>
2025-04-02 23:40:53 +0900

    저장

--- users/views.py
+++ users/views.py
@@ -1,2 +1,22 @@
-from django.shortcuts import render
-# test
+# users/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.hashers import make_password
+from .models import Users
+from rest_framework.permissions import AllowAny
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        data = request.data
+        try:
+            user = Users.objects.create(
+                email=data['email'],
+                username=data['username'],
+                password=make_password(data['password']),
+            )
+            return Response({"message": "회원가입 성공!"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- yorijori/settings.py
+++ yorijori/settings.py
@@ -107,3 +107,17 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
